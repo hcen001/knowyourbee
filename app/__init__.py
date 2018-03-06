2735104984bb88cf060775cdbf749cec05215216
Hector Cen <hcen001@cs.fiu.edu>
2018-03-06 16:37:58 -0500

    Fixed configuration handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__,  instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
 # Load the default configuration
 app.config.from_object('config.default')
