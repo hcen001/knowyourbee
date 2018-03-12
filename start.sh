88c5cb7fe65679ed3827a488c82274257f71bae5
Hector Cen <hcen001@cs.fiu.edu>
2018-03-12 16:06:20 -0400

    Updated start.sh script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LANG=en_US.utf8
+export LC_ALL=en_US.utf8
 export APP_CONFIG_FILE=/home/fabian-a/knowyourbee.org/config/development.py
 export FLASK_APP=run.py
 export FLASK_DEBUG=1
