51c58755e89b1756c51ca8deee6c483f31a08a77
Hector Cen <hcen001@cs.fiu.edu>
2018-03-13 11:06:57 -0400

    Added migrate.sh script to ease migration

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+message=$1
+
 export APP_CONFIG_FILE=/home/fabian-a/knowyourbee.org/config/development.py
 export FLASK_APP=run.py
 export FLASK_DEBUG=1
-flask run --host=0.0.0.0
+flask db migrate -m "$message"
