0d733b4dcc479896fb0aa4675de89723cbb36414
yingxin <liyx8610@gmail.com>
2018-05-16 08:59:44 -0400

    add operation to modal window

--- app/templates/config/accreq/index.js
+++ app/templates/config/accreq/index.js
@@ -1,23 +1,5 @@
 updateMenu('#accreq');
 
-function approveAccountRequest(ele,approve){
-    $this = $(ele);
-    $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
-    $.ajax({
-        url: $SCRIPT_ROOT+'/config/accreq/approveAccount/'+$this.data('accreqid'),
-        type: 'POST',
-        dataType: "json",
-        contentType:"application/json",
-        data: JSON.stringify({"approve": approve}),
-    })
-    .done(function(result) {
-        if(result.data===1){
-            window.location.href=$SCRIPT_ROOT+'/config/accreq';
-        }
-    });
-    
-};
-
 var initTable = function () {
 
     var table = $('#accreq_tbl');
@@ -72,8 +54,10 @@ var initTable = function () {
             {"data": "granted",
                 render: function(data, type, row, meta){
                     if(data === false){
-                        data = '<a href="#" style="margin-right:20px" data-accreqid="'+row['id']+'" onclick="approveAccountRequest(this,true)">Approve</a>';
-                        data += '<a href="#" data-accreqid="'+row['id']+'" onclick="approveAccountRequest(this,false)">Reject</a>';
+                        //data = '<a href="#" style="margin-right:20px" data-accreqid="'+row['id']+'" onclick="approveAccountRequest(this,true)">Approve</a>';
+                        //data += '<a href="#" data-accreqid="'+row['id']+'" onclick="approveAccountRequest(this,false)">Reject</a>';
+                        data = '<a class="btn green btn-outline sbold" data-accreqid="'+row['id']+'" data-toggle="modal" href="#approveModel"> Approve </a>';
+                        data += '<a class="btn red btn-outline sbold" data-accreqid="'+row['id']+'" data-toggle="modal" href="#rejectModel"> Reject </a>';
                     }
                     return data;
                 }
@@ -85,6 +69,44 @@ var initTable = function () {
     $('#sample_3_tools > li > a.tool-action').on('click', function() {
         var action = $(this).attr('data-action');
         oTable.DataTable().button(action).trigger();
+    });
+
+    function approveAccountRequest(accreqid,approve){
+
+        $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
+        $.ajax({
+            url: $SCRIPT_ROOT+'/config/accreq/approveAccount/'+accreqid,
+            type: 'POST',
+            dataType: "json",
+            contentType:"application/json",
+            data: JSON.stringify({"approve": approve}),
+        })
+        .done(function(result) {
+            if(result.data===1){
+                window.location.href=$SCRIPT_ROOT+'/config/accreq';
+            }
+        });
+        
+    };
+
+    $('#approveModel').on('shown.bs.modal', function (event) {
+      $btn = $(event.relatedTarget);
+      $('#acceptBtn').data('accreqid', $btn.data('accreqid'));
+    });
+
+    $('#rejectModel').on('shown.bs.modal', function (event) {
+      $btn = $(event.relatedTarget);
+      $('#rejectBtn').data('accreqid', $btn.data('accreqid'));
+    });
+
+    $('#acceptBtn').on('click', function(event) {
+        event.preventDefault();
+        approveAccountRequest($(this).data('accreqid'),true);
+    });
+
+    $('#rejectBtn').on('click', function(event) {
+        event.preventDefault();
+        approveAccountRequest($(this).data('accreqid'),false);
     });
 
 }
