7f7803979e1be86bdec5e58ddc9f7029978891ba
Hector Cen <hcen001@cs.fiu.edu>
2018-05-22 11:46:53 -0400

    modified start.sh script to include env var that points to conf file

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export APP_CONFIG_FILE=/home/fabian-a/knowyourbee.org/config/development.py
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
 export FLASK_APP=run.py
 export FLASK_DEBUG=1
 flask run --host=0.0.0.0
